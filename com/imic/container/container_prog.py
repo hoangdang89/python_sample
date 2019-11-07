83e916eba688738c85bfbc56c088ea9816724e93
Hoàng Đặng <hoangdang89@users.noreply.github.com>
2019-11-07 20:32:04 +0700

    #1 exercise for container.

--- com/imic/container/container_prog.py
+++ com/imic/container/container_prog.py
@@ -1,12 +1,3 @@
 from com.imic.container.crate import Crate
 
-crate1 = Crate(440, 120, 254);
-crate2 = Crate(440, 120, 254);
-crate3 = Crate(440, 120, 254);
-crate4 = Crate(440, 120, 254);
-crate5 = Crate(440, 120, 254);
-crate6 = Crate(440, 120, 254);
-crate7 = Crate(440, 120, 254);
-crate8 = Crate(440, 120, 254);
-
-print(crate1)
+#1. Calculate volume of all crates from create.csv
